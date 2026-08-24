d573dbd34ade777d55030a6b7fce81897b6acc27
chenhaijun20260824 <2765455807@qq.com>
2026-08-24 20:52:59 +0800

    Bump SW cache v2 + auto-update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 玫瑰按摩 - Service Worker (PWA 离线支持)
-const CACHE_NAME = 'rose-massage-v1';
+const CACHE_NAME = 'rose-massage-v2';
 const urlsToCache = [
   '/rose-massage/',
   '/rose-massage/index.html',
